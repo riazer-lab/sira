25aa46380fb468b6768693a79416bf40642f8788
Ricco Xie <ricco@riccox.com>
2023-01-22 17:35:53 +0800

    fix: button loading variant, playground select title naming error feat: add button component docs, completed components overview page perf: prompt built-in `bw` color meaning
    Signed-off-by: Ricco Xie <ricco@riccox.com>

---
+++ apps/website/components/ComponentOverview.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+
+export const ComponentOverviewCard = ({
+  component,
+  name,
+  href,
+}: {
+  component: ReactNode;
+  name: string;
+  href: string;
+}) => {
+  return (
+    <Link
+      href={href}
+      className={`rounded-2xl has-border flex flex-col overflow-hidden hover:!no-underline bg-bw-50 group hover:border-primary-600`}
+    >
+      <div
+        className={`flex items-center text-xl font-bold px-6 py-3 bg-bw-100/30 text-bw-1000 row-span-full group-hover:underline`}
+      >
+        {name}
+      </div>
+      <div
+        className={`flex-1 flex justify-center items-center gap-2 px-6 py-4 has-border !border-x-0 !border-b-0`}
+      >
+        {component}
+      </div>
+    </Link>
+  );
+};
