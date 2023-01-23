cce140bf896ded03b51fbc1da4f14983a5f73ead
Ricco Xie <ricco@riccox.com>
2023-01-23 22:46:54 +0800

    docs: completed existed components documents perf: cornered shape naming & `sm` size params of badge component. fix: loader bar-bounce component visibility. fix: docs site header logo wrong height.
    Signed-off-by: Ricco Xie <ricco@riccox.com>

--- apps/website/components/Logo.tsx
+++ apps/website/components/Logo.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const Logo = ({ className = "" }: Props) => {
   return (
     <span className={`${className} flex items-center gap-3`}>
-      <LogoIcon className={`w-5`} />
+      <LogoIcon className={`w-5 h-fit`} />
       <span className={"font-bold text-xl"}>Sira</span>
     </span>
   );
---
+++ apps/website/components/demo/drawer.tsx
@@ -0,0 +1,61 @@
+import _ from "lodash";
+import React, { useCallback, useState } from "react";
+
+interface Props {
+  position: "top" | "bottom" | "left" | "right";
+  labelText?: string;
+  content?: string;
+
+  pauseScroll?: boolean;
+  overlay?: boolean;
+  overlayClickClose?: boolean;
+}
+
+export const DrawerDemo = ({
+  position = "left",
+  pauseScroll = false,
+  overlay = true,
+  overlayClickClose = true,
+  labelText = "Open Drawer",
+  content = "Your content",
+}: Props) => {
+  const [drawerId] = useState(_.uniqueId("sira-demo-drawer-"));
+
+  const toggleDrawer = useCallback(() => {
+    let ele = document.getElementById(drawerId);
+    let show = ele!.classList.contains("show");
+    if (show) {
+      ele!.classList.remove("show");
+    } else {
+      ele!.classList.add("show");
+    }
+  }, [drawerId]);
+
+  return (
+    <div>
+      <label className="btn solid success w-fit" onClick={toggleDrawer}>
+        {labelText}
+      </label>
+      {overlay && (
+        <label
+          className="drawer-overlay"
+          onClick={overlayClickClose ? toggleDrawer : () => {}}
+        ></label>
+      )}
+      <div
+        className={`drawer ${position} ${pauseScroll ? "pause-scroll" : ""}`}
+        id={drawerId}
+      >
+        <div className="content flex flex-col h-full">
+          <label
+            className="btn sm circle ghost absolute right-2 top-2"
+            onClick={toggleDrawer}
+          >
+            ✕
+          </label>
+          <h2 className="text-xl">{content}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
