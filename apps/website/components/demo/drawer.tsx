90d0116e2efd27d3f540b134ef79eee36adc074a
Ricco Xie <ricco@riccox.com>
2023-07-04 17:01:12 +0800

    docs: add class table for divider & drawer docs.

--- apps/website/components/demo/drawer.tsx
+++ apps/website/components/demo/drawer.tsx
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 interface Props {
   position: 'top' | 'bottom' | 'left' | 'right';
@@ -11,6 +11,17 @@ interface Props {
   overlayClickClose?: boolean;
 }
 
+const toggleDrawer = (drawerId: string) => {
+  let ele = document.getElementById(drawerId);
+  console.log(drawerId, ele);
+  let show = ele!.classList.contains('show');
+  if (show) {
+    ele!.classList.remove('show');
+  } else {
+    ele!.classList.add('show');
+  }
+};
+
 export const DrawerDemo = ({
   position = 'left',
   pauseScroll = false,
@@ -19,27 +30,19 @@ export const DrawerDemo = ({
   labelText = 'Open Drawer',
   content = 'Your content',
 }: Props) => {
-  const [drawerId] = useState(_.uniqueId('sira-demo-drawer-'));
-
-  const toggleDrawer = useCallback(() => {
-    let ele = document.getElementById(drawerId);
-    let show = ele!.classList.contains('show');
-    if (show) {
-      ele!.classList.remove('show');
-    } else {
-      ele!.classList.add('show');
-    }
-  }, [drawerId]);
+  const drawerId = _.uniqueId('sira-demo-drawer-');
 
   return (
     <div>
-      <label className="btn solid success w-fit" onClick={toggleDrawer}>
+      <label className="btn solid success w-fit" onClick={() => toggleDrawer(drawerId)}>
         {labelText}
       </label>
-      {overlay && <label className="drawer-overlay" onClick={overlayClickClose ? toggleDrawer : () => {}}></label>}
+      {overlay && (
+        <label className="drawer-overlay" onClick={overlayClickClose ? () => toggleDrawer(drawerId) : () => {}}></label>
+      )}
       <div className={`drawer ${position} ${pauseScroll ? 'pause-scroll' : ''}`} id={drawerId}>
         <div className="content flex flex-col h-full">
-          <label className="btn sm circle ghost absolute right-2 top-2" onClick={toggleDrawer}>
+          <label className="btn sm circle ghost absolute right-2 top-2" onClick={() => toggleDrawer(drawerId)}>
             ✕
           </label>
           <h2 className="text-xl">{content}</h2>
